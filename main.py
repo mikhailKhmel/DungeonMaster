ed6b765d87d0ef2d7339cebd9d35afb7bc75bd54
Georgiy Kosichkin <43862355+GeorgiyKos@users.noreply.github.com>
2018-10-07 17:27:58 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import logSystem
 import random
 from renderGame import *
+from invControl import *
 
 # здесь определяются константы, классы и функции
 FPS = 60
@@ -26,106 +27,6 @@ sc.fill((0,0,0))
 
 clock = pygame.time.Clock()
 
-def loadInv():
-	f = open('inv.txt', 'r')
-	s = f.read()
-	m = []
-	tmp = []
-	for x in range(0,len(s)):
-		if s[x]=='\n':
-			m.append(tmp)
-			tmp = []
-		else:
-			tmp.append(s[x])
-	print(m)
-	return m
-	
-
-
-def renderInv(inv):
-	
-	x = 0
-	y = 0
-	for i in range(0,len(inv)):
-		for j in range(0,len(inv[i])):
-			if inv[i][j]=='0':
-				img0 = pygame.image.load('srcBMP/inv/border.bmp')
-				img0_rect = img0.get_rect(topleft=(x,y))
-				surfInv.blit(img0,img0_rect)
-				x+=STEP
-			elif inv[i][j]=='1':
-				img1 = pygame.image.load('srcBMP/inv/empty.bmp')
-				img1_rect = img1.get_rect(topleft=(x,y))
-				surfInv.blit(img1,img1_rect)
-				x+=STEP
-			elif inv[i][j]=='2':
-				img2 = pygame.image.load('srcBMP/inv/candle.bmp')
-				img2_rect = img2.get_rect(topleft=(x,y))
-				surfInv.blit(img2,img2_rect)
-				x+=STEP
-			elif inv[i][j]=='3':
-				img3 = pygame.image.load('srcBMP/inv/potion.bmp')
-				img3_rect = img3.get_rect(topleft=(x,y))
-				surfInv.blit(img3,img3_rect)
-				x+=STEP
-			elif inv[i][j]=='4':
-				img4 = pygame.image.load('srcBMP/inv/selector.bmp')
-				img4_rect = img4.get_rect(topleft=(x,y))
-				surfInv.blit(img4,img4_rect)
-				x+=STEP
-		x=0
-		y+=STEP
-	sc.blit(surfInv,(GAME_HEIGHT,0))
-		
-def openInv():
-	
-	inv[len(inv)-2][len(inv)] = inv[1][1]
-	inv[1][1] = '4'
-	renderInv(inv)
-	while True:
-		for i in pygame.event.get():
-			if i.type == pygame.QUIT:
-				exit()
-			
-			elif i.type == pygame.KEYDOWN:
-				if i.key == pygame.K_i:
-					#surfGameDark.set_alpha(0)
-					return
-				elif i.key == pygame.K_e:
-					inv[len(inv)-2][len(inv)] = '1'
-				elif i.key == pygame.K_1:
-					exit()
-				if i.key == pygame.K_UP:
-					moveInv(-1,0,inv)
-				elif i.key == pygame.K_RIGHT:
-					moveInv(0,1,inv)
-				elif i.key == pygame.K_DOWN:
-					moveInv(1,0,inv)
-				elif i.key == pygame.K_LEFT:
-					moveInv(0,-1,inv)
-				else:
-					print('ERROR KEY')
-
-		renderInv(inv)
-		pygame.display.update()
-
-
-def moveInv(dx,dy,inv):
-	x=0
-	y=0
-	for i in range(0,len(inv)):
-		for j in range(0,len(inv[i])):
-			if (inv[i][j]=='4'):
-				x=i
-				y=j
-				break
-	if inv[x+dx][y+dy] == '0':
-		pass
-	else:
-		inv[x][y] = inv[len(inv)-2][len(inv)]
-		inv[len(inv)-2][len(inv)] = inv[x+dx][y+dy]
-		inv[x+dx][y+dy] = '4'
-
 # если надо до цикла отобразить объекты на экране
 
 maps = loadMap(player['level'])
@@ -140,7 +41,7 @@ renderMap(maps,player,randPlitka,sc)
 logSystem.scanLog(maps,player,sc)
 
 inv = loadInv()
-renderInv(inv)
+renderInv(inv,sc)
 
 pygame.display.update()
  
@@ -168,8 +69,7 @@ while True:
 				tmp = maps
 				maps = renderList(0,-1,level,tmp,player)
 			elif i.key == pygame.K_i:
-				openInv()
-				#
+				openInv(inv,sc)
 			else:
 				print('ERROR KEY')
 
